7686519f58f7af7f2976f8c6f2c22495f3a02ec2
Lucas Oliveira Santos <lucas.santos@forseti.com.br>
2019-09-24 10:30:23 -0300

    Subindo repositório

---
+++ busca-sj.php
@@ -0,0 +1,20 @@
+<?php
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client();
+$resposta = $client->request("GET", "https://www.spacejam.com/archive/spacejam/movie/cmp/jamcentral/photos.html");
+
+$html = $resposta->getBody()->getContents();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$subtitle = $crawler->filterXPath("//blockquote/font[1]");
+
+foreach ($subtitle as $subtitulo)
+{
+    var_dump($subtitulo->textContent);
+}
