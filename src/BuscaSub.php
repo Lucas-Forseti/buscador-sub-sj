b86a017d7a590ad5e83e112354ec4d5cecdd32b3
Lucas Oliveira Santos <lucas.santos@forseti.com.br>
2019-10-02 15:17:09 -0300

    Instaciado httpClient e crawler no construtor/classe

--- src/BuscaSub.php
+++ src/BuscaSub.php
@@ -9,8 +9,11 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class BuscaSub
 {
+    private $httpClient;
+    private $crawler;
     public function __construct(ClientInterface $httpClient, Crawler $crawler)
     {
-
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
     }
 }
