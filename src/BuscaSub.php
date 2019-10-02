96c0cd95e450ae126959ab7a0d9ea2425c8b933e
Lucas Oliveira Santos <lucas.santos@forseti.com.br>
2019-10-02 15:13:14 -0300

    Adicionado construtor com httpClient e crawler

--- src/BuscaSub.php
+++ src/BuscaSub.php
@@ -4,7 +4,13 @@
 namespace Forseti\BuscaSJ;
 
 
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
 class BuscaSub
 {
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
 
+    }
 }
