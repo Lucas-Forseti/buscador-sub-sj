a553932646ee57511dee3213932edf25a5cd50e7
Lucas Oliveira Santos <lucas.santos@forseti.com.br>
2019-10-03 10:48:29 -0300

    Adicionado use da classe de busca

--- busca-sj.php
+++ busca-sj.php
@@ -2,13 +2,14 @@
 require 'vendor/autoload.php';
 require 'src/BuscaSub.php';
 
+use Forseti\BuscaSJ\BuscaSub;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client(['base_uri' => 'https://www.spacejam.com/']);
 $crawler = new Crawler();
 
-$buscador = new \Forseti\BuscaSJ\BuscaSub($client, $crawler);
+$buscador = new BuscaSub($client, $crawler);
 $subtitle = $buscador->buscar('archive/spacejam/movie/cmp/jamcentral/photos.html');
 
 foreach ($subtitle as $subtitulo)
